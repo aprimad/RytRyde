26ad204bcfadaa4baafe978f3ce841e5134248ee
Siddhant Khobragade <siddhantdrk@gmail.com>
2021-03-28 22:47:43 +0530

    order 2 New branch with drawables error fixed

---
+++ app/src/main/java/com/example/rytryde/AddVehicleDetailsActivity.java
@@ -0,0 +1,14 @@
+package com.example.rytryde;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class AddVehicleDetailsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_vehicle_details);
+    }
+}
